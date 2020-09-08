3c8798fe97310200d0698a49ff5f1dd1b6474b0d
williakg1 <69922976+williakg1@users.noreply.github.com>
2020-09-08 17:25:09 -0400

    Add files via upload

---
+++ HypotCalc2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HypotCalc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			
+				double a = 0.0;
+				double b = 0.0;
+				double c = 0.0;
+				string temp;
+				//read in the miles
+
+				Console.WriteLine("Enter a");
+				temp = Console.ReadLine();
+				a = Double.Parse(temp);
+				//read in the gallons
+				Console.WriteLine("Enter b");
+				temp = Console.ReadLine();
+				b = Double.Parse(temp);
+				//compute the miles per gallons
+				c = Math.Sqrt((a * a) + (b * b));
+				Console.WriteLine("C is equal to " + c);
+			
+		}
+    }
+}
